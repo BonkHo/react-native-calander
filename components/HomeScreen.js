e11cfb69810941cbf32da41e2fdd409481437ba4
John <John@DESKTOP-SRO1BKN>
2021-09-18 23:27:04 -0700

    Added a pressable component

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { Text, View, Button } from "react-native";
-import { Box } from "native-base";
+import { Text, Alert } from "react-native";
+import { Pressable } from "native-base";
+import { NativeBaseProvider } from "native-base";
 
 const HomeScreen = ({ navigation }) => {
     return (
@@ -12,7 +13,19 @@ const HomeScreen = ({ navigation }) => {
             >
                 Home Page
             </Text>
-            <Box></Box>
+            <Pressable
+                onPress={() => Alert.alert("hello")}
+                p={2}
+                borderWidth={1}
+                _light={{
+                    borderColor: "dark.200",
+                }}
+                _dark={{
+                    borderColor: "dark.600",
+                }}
+            >
+                <Text>Hello</Text>
+            </Pressable>
         </NativeBaseProvider>
     );
 };
